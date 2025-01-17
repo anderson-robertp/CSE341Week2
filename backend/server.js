3ce637a71de333a9681a416a1e5ae6c1935be794
anderson-robertp <anderon-rp@hotmail.com>
2025-01-16 22:30:05 -0600

    backend presenting data

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 //const path = require('path');
 const profRoute = require('./routes/profRoute');
+const profController = require('./controllers/profController');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json()); // Parse JSON request bodies
-app.use('/professional', profRoute);
+//app.use('/professional', profRoute);
 app.use('/api', profRoute);
+
+app.get('/professional', profController.getUser);
 
 //app.use(express.static(path.join(__dirname, 'public')));
 
