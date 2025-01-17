4f10eb4b400f29cba2e9ea6884bd2e386e1a4f00
anderson-robertp <anderon-rp@hotmail.com>
2025-01-16 22:42:21 -0600

    backend update

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 //const path = require('path');
 const profRoute = require('./routes/profRoute');
 const profController = require('./controllers/profController');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors()); // Enable CORS
 app.use(express.json()); // Parse JSON request bodies
 //app.use('/professional', profRoute);
 app.use('/api', profRoute);
