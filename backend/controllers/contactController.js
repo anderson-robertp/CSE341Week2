23ad358b32cd251020abfc906ae67fd458daa4fc
anderson-robertp <anderon-rp@hotmail.com>
2025-01-21 21:10:49 -0600

    working functions

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,6 +1,6 @@
 const { response } = require('express');
 const mongodb = require('../db/connect');
-//const {ObjectId} = require('mongodb').ObjectId;
+const { ObjectId } = require('mongodb');
 
 const getAllData = async (req, res, next) => {
   const result = await mongodb.getDb().collection('contacts').find();
@@ -52,27 +52,42 @@ const createContact = async (req, res, next) => {
 };
 
 const updateContact = async (req, res, next) => {
-  const contactId = req.params.id;
-  const updatedContact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    faovriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+  try {
+    const contactId = req.params.id;
+
+    const updatedContact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      faovriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const result = await mongodb.getDb().collection('contacts').replaceOne({ _id: new ObjectId (contactId) }, updatedContact);
+    
+    console.log(result); // for testing
+    
+    if (result.matchedCount === 0) {
+      res.status(404).json({ message: 'Contact not found' });
+    } else if (result.modifiedCount > 0) {
+      res.status(200).json({ message: 'Contact updated successfully' });
+    } else {
+      res.status(304).json({ message: 'No changes made to the contact' });
+    }
+
+  } catch (err) {
+    console.error('Error updating contact:', err.message);
+    res.status(500).json({ message: 'An error occurred', error: err.message });
   }
-  const result = await mongodb.getDb().collection('contacts').replaceOne({ _id: contactId }, updatedContact);
-  console.log(result); // for testing
-  if (result.modifiedCount > 0) {
-    res.status(204).json({ message: 'Contact updated successfully' });
-  } else {
-    res.status(500).json({ message: 'Error updating contact' });
-  }
-}; // to be implemented
+};
 
 const deleteContact = async (req, res, next) => {
   const contactId = req.params.id;
-  const result = await mongodb.getDb().collection('contacts').deleteOne({ _id: contactId });
+
+  const result = await mongodb.getDb().collection('contacts').deleteOne({ _id: new ObjectId (contactId) });
+
   console.log(result); // for testing
+
   if (result.deletedCount > 0) {
     res.status(200).json({ message: 'Contact deleted successfully' });
   } else {
